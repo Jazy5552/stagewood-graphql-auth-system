2a2a59454367e66273ac95249013eecafee38430
Jazy Llerena <jazy@jazyserver.com>
2020-07-05 23:44:29 +0000

    Add user self info graph

--- stagewood-auth-system-backend/src/resolvers/Query.js
+++ stagewood-auth-system-backend/src/resolvers/Query.js
@@ -1,3 +1,5 @@
+const { getUserId } = require("../utils");
+
 function info() {
   return 'This is the API for the stagewood test project';
 }
@@ -7,11 +9,26 @@ function feed(parent, args, context) {
 }
 
 function link(parent, args, context) {
-  return context.prisma.link.findOne({ where: { id: parseInt(args.id) }});
+  return context.prisma.link.findOne({
+    where: {
+      id: parseInt(args.id),
+    },
+  });
+}
+
+function user(parent, args, context) {
+  const userId = getUserId(context);
+
+  return context.prisma.user.findOne({
+    where: {
+      id: userId,
+    },
+  });
 }
 
 module.exports = {
   info,
   feed,
   link,
+  user,
 }
