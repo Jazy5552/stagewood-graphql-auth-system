10003441e73fbcada3357dce0a5e4a632bffd27c
Jazy Llerena <jazy@jazyserver.com>
2020-07-05 18:19:08 +0000

    Implement simple apollo data fetching on the frontend

---
+++ stagewood-auth-system-frontend/src/components/LinkList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Link from './Link';
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      url
+      description
+    }
+  }
+`;
+
+class LinkList extends Component {
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error: {error}</div>
+
+          const linksToRender = data.feed;
+
+          return (
+            <div>
+              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default LinkList;
