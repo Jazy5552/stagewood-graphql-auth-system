0258b4c5335d93064921a0279d37a6a4fcdf2a85
Jazy Llerena <jazy@jazyserver.com>
2020-07-05 22:44:30 +0000

    Fix typo in required paths

--- stagewood-auth-system-backend/src/index.js
+++ stagewood-auth-system-backend/src/index.js
@@ -1,9 +1,9 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { PrismaClient } = require('@prisma/client');
-const Query = require('resolvers/Query');
-const Mutation = require('resolvers/Mutation');
-const User = require('resolvers/User');
-const Link = require('resolvers/Link');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
 
 const resolvers = {
   Query,
