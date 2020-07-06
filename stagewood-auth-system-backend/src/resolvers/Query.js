a784be8739b909701ab386467c97a23cb7dc8f61
Jazy Llerena <jazy@jazyserver.com>
2020-07-06 18:59:16 +0000

    Implement feeds count

--- stagewood-auth-system-backend/src/resolvers/Query.js
+++ stagewood-auth-system-backend/src/resolvers/Query.js
@@ -4,7 +4,7 @@ function info() {
   return 'This is the API for the stagewood test project';
 }
 
-function feed(parent, args, context) {
+async function feed(parent, args, context) {
   // Hope prisma sanitizes where input
   const where = args.filter
     ? {
@@ -15,11 +15,20 @@ function feed(parent, args, context) {
     }
     : {};
 
-  return context.prisma.link.findMany({
+  const links = await context.prisma.link.findMany({
     where,
     skip: args.skip,
     take: args.take,
   });
+
+  const count = await context.prisma.link.count({
+    where,
+  });
+
+  return {
+    links,
+    count,
+  }
 }
 
 function link(parent, args, context) {
