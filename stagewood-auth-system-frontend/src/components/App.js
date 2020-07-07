dbef200ce7e76651441e36fdb75c60715b7f9e21
Jazy Llerena <jazy@jazyserver.com>
2020-07-07 01:56:58 +0000

    Add the authorization token to the apollo client headers

--- stagewood-auth-system-frontend/src/components/App.js
+++ stagewood-auth-system-frontend/src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import LinkList from 'components/LinkList';
 import Error404 from 'components/errors/Error404';
 import Header from 'components/Header';
 import { Switch, Route } from 'react-router-dom';
--- stagewood-auth-system-frontend/src/index.js
+++ stagewood-auth-system-frontend/src/index.js
@@ -7,15 +7,27 @@ import * as serviceWorker from './serviceWorker';
 
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { setContext } from 'apollo-link-context';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { AUTH_TOKEN } from './constants';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((request, {headers}) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
